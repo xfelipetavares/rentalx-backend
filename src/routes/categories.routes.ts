764d5003463fff364b8d87b36ee6af43249f4b06
Felipe Tavares <xfelipetavares@gmail.com>
2024-02-05 23:29:58 +0000

    added name checker

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,9 +7,19 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body
 
+  if(categoriesRepository.findByName(name)){
+    return response.status(400).json({ error: "Category already exists." })
+  }
+
   categoriesRepository.create({ name, description })
 
   return response.status(201).send()
+})
+
+categoriesRoutes.get('/', (request, response) => {
+  const result = categoriesRepository.list()
+
+  return response.json(result)
 })
 
 export { categoriesRoutes }
