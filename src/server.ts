d65d50d1f3cee3ff9b509c34d938f850ffadb35e
Felipe Tavares <xfelipetavares@gmail.com>
2024-02-22 16:40:30 +0000

    create database

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import { router } from "./routes"
 import swaggerUI from "swagger-ui-express"
 import swaggerFile from "./swagger.json"
+import "./database"
 
 const app = express()
 
