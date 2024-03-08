bfc7b74fb40e1940e649d8da3dd30b042544b14f
Felipe Tavares <xfelipetavares@gmail.com>
2024-03-08 18:04:37 +0000

    fix some bugs on those routes

--- src/modules/cars/useCases/importCategories/ImportCategoriesController.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesController.ts
@@ -8,7 +8,7 @@ class ImportCategoriesController {
     const importCategoriesUseCase = container.resolve(ImportCategoriesUseCase)
     await importCategoriesUseCase.execute(file)
 
-    return response.send()
+    return response.status(201).send()
   }
 }
 
