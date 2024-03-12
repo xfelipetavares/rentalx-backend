6d70be52c60ae90dc38a0114431ecfe26899e814
Felipe Tavares <xfelipetavares@gmail.com>
2024-03-12 19:04:02 +0000

    refactor migrations

--- src/database/migrations/1710268503280-CreateCategories.ts
+++ src/database/migrations/1710268503280-CreateCategories.ts
@@ -1,40 +1,23 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
-export class CreateUsers1710095088534 implements MigrationInterface {
+export class CreateCategories1710268503280 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "users",
+        name: "categories",
         columns: [
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
             type: "varchar",
           },
           {
-            name: "username",
+            name: "description",
             type: "varchar",
-            isUnique: true,
-          },
-          {
-            name: "password",
-            type: "varchar",
-          },
-          {
-            name: "email",
-            type: "varchar",
-          },
-          {
-            name: "driver_license",
-            type: "varchar",
-          },
-          {
-            name: "isAdmin",
-            type: "boolean",
-            default: false,
           },
           {
             name: "created_at",
@@ -47,6 +30,6 @@ export class CreateUsers1710095088534 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("users")
+    await queryRunner.dropTable("categories")
   }
 }
--- src/database/migrations/1710268550834-CreateSpecifications.ts
+++ src/database/migrations/1710268550834-CreateSpecifications.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
-export class CreateSpecification21709745460709 implements MigrationInterface {
+export class CreateSpecifications1710268550834 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
